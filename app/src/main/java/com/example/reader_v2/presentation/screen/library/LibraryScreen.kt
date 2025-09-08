2dc5d7514448a7ee49c8347f1c6a403eb8793c75
hoang <hoangle.ntkh@gmail.com>
2025-09-08 15:43:52 +0700

    add file picker functionality to LibraryScreen.kt

--- app/src/main/java/com/example/reader_v2/presentation/screen/library/LibraryScreen.kt
+++ app/src/main/java/com/example/reader_v2/presentation/screen/library/LibraryScreen.kt
@@ -1,5 +1,7 @@
 package com.example.reader_v2.presentation.screen.library
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -7,18 +9,35 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 
 @Composable
 fun LibraryScreen(
 
 ) {
+	val scope = rememberCoroutineScope()
+	val filePickerScreenLauncher = rememberLauncherForActivityResult(
+		contract = ActivityResultContracts.OpenDocument(),
+		onResult = { uri ->
+			uri?.let {
+				scope.launch {
+
+				}
+			}
+		}
+	)
+
+
 	Scaffold(
 		modifier = Modifier.fillMaxSize(),
 	) {
 		innerPadding ->
 		Box(modifier = Modifier.padding(innerPadding)) {
-			Button() {
+			Button(
+				onClick = { filePickerScreenLauncher.launch(arrayOf("application/epub+zip")) }
+			) {
 				Text(text = "Select Epub File")
 			}
 		}
