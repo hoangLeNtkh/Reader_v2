b9d5396bf38c061fa77f8139387d1cfbfee27b47
hoang <hoangle.ntkh@gmail.com>
2025-09-08 15:04:08 +0700

    create AppNavGraph to define app navigation flow

--- app/src/main/java/com/example/reader_v2/MainActivity.kt
+++ app/src/main/java/com/example/reader_v2/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.reader_v2.presentation.AppNavGraph
 import com.example.reader_v2.ui.theme.Readerv2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,9 @@ class MainActivity : ComponentActivity() {
 		enableEdgeToEdge()
 		setContent {
 			Readerv2Theme {
-				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Greeting(
-						name = "Android",
-						modifier = Modifier.padding(innerPadding)
-					)
-				}
+				AppNavGraph()
 			}
 		}
 	}
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-	Readerv2Theme {
-		Greeting("Android")
-	}
-}
