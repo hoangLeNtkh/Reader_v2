2c2418b017898b61f01e8baac6bae4109c5abb95
hoang <hoangle.ntkh@gmail.com>
2025-09-09 13:04:44 +0700

    setup app database

--- app/src/main/java/com/example/reader_v2/data/AppDatabase.kt
+++ app/src/main/java/com/example/reader_v2/data/AppDatabase.kt
@@ -1,4 +1,34 @@
 package com.example.reader_v2.data
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.reader_v2.data.dao.BookDao
+import com.example.reader_v2.data.entity.BookEntity
+
+@Database(
+	entities = [BookEntity::class],
+	version = 1,
+	exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+	abstract fun bookDao(): BookDao
+
+	companion object {
+		@Volatile
+		private var INSTANCE: AppDatabase? = null
+
+		fun getDatabase(context: Context): AppDatabase {
+			return INSTANCE ?: synchronized(this) {
+				val instance = Room.databaseBuilder(
+					context.applicationContext,
+					AppDatabase::class.java,
+					"app_database"
+				).build()
+				INSTANCE = instance
+				instance
+			}
+		}
+	}
 }
