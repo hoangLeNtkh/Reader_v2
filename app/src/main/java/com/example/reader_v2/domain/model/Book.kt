cfd6c9fb8f0e6f6fdec0747320dbfcdddc005e73
hoang <hoangle.ntkh@gmail.com>
2025-09-09 13:14:16 +0700

    fill book domain model class

--- app/src/main/java/com/example/reader_v2/domain/model/Book.kt
+++ app/src/main/java/com/example/reader_v2/domain/model/Book.kt
@@ -1,4 +1,10 @@
 package com.example.reader_v2.domain.model
 
-class Book {
-}
+data class Book(
+	val id: String,
+	val filePath: String,
+	val title: String,
+	val author: String?,
+	val description: String?,
+	val totalChapters: Int,
+)
