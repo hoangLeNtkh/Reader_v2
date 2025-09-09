2d7b429a0d050f1ed6d72a59978c512f543d7e1a
hoang <hoangle.ntkh@gmail.com>
2025-09-09 12:57:47 +0700

    add basic query methods for book dao

--- app/src/main/java/com/example/reader_v2/data/dao/BookDao.kt
+++ app/src/main/java/com/example/reader_v2/data/dao/BookDao.kt
@@ -1,4 +1,24 @@
 package com.example.reader_v2.data.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.reader_v2.data.entity.BookEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface BookDao {
+	@Query("SELECT * FROM books")
+	fun getAllBooks(): Flow<List<BookEntity>>
+
+	@Query("SELECT * FROM books WHERE id = :bookId")
+	fun getBookById(bookId: String): Flow<BookEntity?>
+
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	fun insertBook(book: BookEntity)
+
+	@Delete
+	fun deleteBook(book: BookEntity)
 }
