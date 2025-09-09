ae156c2edd205e1a2ae8879f9405ead7723ef1ff
hoang <hoangle.ntkh@gmail.com>
2025-09-09 12:54:24 +0700

    create essential classes, create book entity

--- app/src/main/java/com/example/reader_v2/data/entity/BookEntity.kt
+++ app/src/main/java/com/example/reader_v2/data/entity/BookEntity.kt
@@ -1,4 +1,15 @@
 package com.example.reader_v2.data.entity
 
-class BookEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "books")
+data class BookEntity(
+	@PrimaryKey
+	val id: String,
+	val filePath: String,
+	val title: String,
+	val author: String?,
+	val description: String?,
+	val totalChapters: Int,
+)
