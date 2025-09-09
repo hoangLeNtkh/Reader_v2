6ddd6037b04dfe878b5187cd796c7d6562080f2f
hoang <hoangle.ntkh@gmail.com>
2025-09-09 13:08:47 +0700

    setup book dao and app database dependencies's module

--- app/src/main/java/com/example/reader_v2/MainAppModule.kt
+++ app/src/main/java/com/example/reader_v2/MainAppModule.kt
@@ -1,4 +1,22 @@
 package com.example.reader_v2
 
-class MainAppModule {
+import android.content.Context
+import com.example.reader_v2.data.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MainAppModule {
+	@Provides
+	@Singleton
+	fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase = AppDatabase.getDatabase(context)
+
+	@Provides
+	@Singleton
+	fun provideBookDao(appDatabase: AppDatabase) = appDatabase.bookDao()
 }
